7c2778b9907c6e1ea50f92bb220c2176a31ef341
DecayQ <decayq@gmail.com>
2019-11-25 11:58:31 +0800

    Site updated: 2019-11-25 11:58:29

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019/10/17/new-minapp-chatmoji/');
+  precacheUrls.push('/2019/09/24/learning-javascript-api-array-from/');
 
   precacheUrls.push('/2019/08/06/xicis-journey-and-happy-birthday-minapp-cloud/');
 
-  precacheUrls.push('/2019/08/19/enhance-hexo-with-pwa-in-three-steps/');
-
-  precacheUrls.push('/2019/08/14/blog-site-with-google-adsense-now/');
+  precacheUrls.push('/2019/11/25/a-way-to-save-large-image-from-canvas/');
 
   precacheUrls.push('/2019/08/16/xicis-next-step/');
+
+  precacheUrls.push('/2019/08/24/xicis-new-feature-xici-lottery-machine/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":20};
