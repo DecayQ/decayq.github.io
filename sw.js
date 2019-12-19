8ad7da0254a96f25ac25e7c08100744e2715fa77
DecayQ <decayq@gmail.com>
2019-12-19 15:34:09 +0800

    Site updated: 2019-12-19 15:34:03

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019/07/24/introducing-xici-in-qq-mini-program/');
+  precacheUrls.push('/2019/12/14/misc-2019-12-14/');
 
   precacheUrls.push('/2019/08/06/xicis-journey-and-happy-birthday-minapp-cloud/');
 
-  precacheUrls.push('/2019/08/16/xicis-next-step/');
-
-  precacheUrls.push('/2019/08/19/enhance-hexo-with-pwa-in-three-steps/');
-
   precacheUrls.push('/2019/08/24/xicis-new-feature-xici-lottery-machine/');
+
+  precacheUrls.push('/2019/11/25/a-way-to-save-large-image-from-canvas/');
+
+  precacheUrls.push('/2019/08/16/xicis-next-step/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":20};
